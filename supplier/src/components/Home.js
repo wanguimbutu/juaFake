8b4b1959662d322fa037c1322f8bfb924cca9f1c
wanguimbutu <wanguimbutu@gmail.com>
2023-10-26 16:12:56 +0300

    Adds new Changes

--- supplier/src/components/Home.js
+++ supplier/src/components/Home.js
@@ -1,10 +1,18 @@
-import React from 'react';
-function Home() {
-return (
-
-    <div>
-<h1>HELLO </h1>
-    </div>
-)
+import { useNavigate } from 'react-router-dom';
+import './Home.css'
+function Home(){
+    const navigate=useNavigate();
+    const navigateToSignup=()=>{
+        navigate('/signup')
+    }
+    return(
+        <div className="hero">
+        <h1 className="text-center">AUTHENTIFY</h1>
+        <h3 className="text-center">Ensuring your drinks are safe, so that you can enjoy</h3>
+       <div className="text-center">
+       <button className="btn btn-primary" onClick={navigateToSignup}>Get Started</button>
+       </div>
+        </div>
+    )
 }
 export default Home;
